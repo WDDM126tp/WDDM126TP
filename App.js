b9db7c56e80760ba29a7d1999fedaa23a267c4e7
WDDM126tp <n01712634@humber.ca>
2024-12-12 17:59:32 -0500

    Update App.js

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to WDDM126TP</h1>
+        <h1>Welcome to Tanu repository WDDM126TP </h1>
         <MyComponent />
       </header>
     </div>
