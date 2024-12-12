6fc3f3e3db0bd7abbd33cd0f1c02b6d95a216405
WDDM126tp <n01712634@humber.ca>
2024-12-12 17:57:14 -0500

    Add files via upload

---
+++ Mycomponent.js
@@ -0,0 +1,14 @@
+// src/components/MyComponent.js
+import React from 'react';
+
+function Mycomponent() {
+  return (
+    <div>
+      <h2>My Component</h2>
+      <p>This is a new component.</p>
+      <p>Hello My name is Tanu Patel</p>
+    </div>
+  );
+}
+
+export default Mycomponent;
