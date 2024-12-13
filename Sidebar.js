717c2a47a9ecae8fbc56d25bbdc74d22606caf7f
WDDM126tp <n01712634@humber.ca>
2024-12-12 21:18:41 -0500

    Added a header for sidebar in Sidebar.js
    Included a heading above the list for clearer organization.

--- Sidebar.js
+++ Sidebar.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const Sidebar = () => {
-    return <aside><ul><li>Item 1</li><li>Item 2</li></ul></aside>;
+    return (
+        <aside>
+            <h2>Sidebar Menu</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </aside>
+    );
 };
 
 export default Sidebar;
