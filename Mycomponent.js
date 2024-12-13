55622bde3f3beac207954188c9f994db26316d48
WDDM126tp <n01712634@humber.ca>
2024-12-12 21:17:33 -0500

    Added a Title Attribute in Mycomponent.js
    Included a title or heading above the main component content for better organization.

--- Mycomponent.js
+++ Mycomponent.js
@@ -1,13 +1,13 @@
-// src/components/MyComponent.js
 import React from 'react';
 
-function Mycomponent() {
+function MyComponent() {
   return (
     <div>
       <h2>My Component</h2>
+      <h3>Component Details</h3>
       <p>This is a new component.</p>
     </div>
   );
 }
 
-export default Mycomponent;
+export default MyComponent;
