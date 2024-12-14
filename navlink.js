24c0b17fdbc2311c7b85f790ae23909f9175d42b
stacy221 <stacytran221@gmail.com>
2024-12-13 23:04:31 -0500

    Added Navigation Link components
    Required for the navigation bar as links.

---
+++ navlink.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default class NavLink extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <a href="#" className="btn btn-lg btn-dark">Link</a>
+            </div>
+        );
+    }
+}
