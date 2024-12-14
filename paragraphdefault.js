1b6320f614acab163ec38ab19af9f5f962bfb338
stacy221 <stacytran221@gmail.com>
2024-12-13 23:07:44 -0500

    Added component with a header and paragraph
    Combined a header and paragraph into one component to be used together.

---
+++ paragraphdefault.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default class ParagraphDefault extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div className="my-5 mx-5">
+                    <h4>Placeholder text</h4>
+                    <p>Insert some text that might belong here.</p>
+                </div>
+            </div>
+        );
+    }
+}
